aaa44c047987142de55a295644f53888fbf53da9
Harry Hedman <harhed-9@student.ltu.se>
2021-01-22 20:17:31 +0100

    First commit

---
+++ inhabitant.py
@@ -0,0 +1,12 @@
+class Inhabitant:
+    def __init__(self, ID, name):
+        self.ID = ID
+        self.name = name        
+        self.money = 0
+        self.hunger = 100
+        self.food = 0
+        self.thirst = 100
+        self.water = 0
+        self.energy = 100
+        self.happiness = 100
+        self.state = "state"
---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+import inhabitant
+
+def main():
+    print("How many people do you want to simulate?")
+    for i in range(int(input())):
+        print(i)
+
+main()
